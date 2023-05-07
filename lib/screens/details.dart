731b4b8fc29ca4cc4341d0dff0f43a4e5dfb341a
Eklu Victor Dziedzorm <55409753+Victorspy-web@users.noreply.github.com>
2023-05-07 16:11:31 +0000

    updated code with tut

---
+++ lib/screens/details.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animations/models/Trip.dart';
+import 'package:flutter_animations/shared/heart.dart';
+// import 'package:lipsum/lipsum.dart' as lipsum;
+import 'package:ipsum/ipsum.dart' as lipsum;
+
+class Details extends StatelessWidget {
+  final Trip trip;
+  const Details({super.key, required this.trip});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        extendBodyBehindAppBar: true,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              ClipRRect(
+                  child: Image.asset(
+                'images/${trip.img}',
+                height: 360,
+                fit: BoxFit.cover,
+                alignment: Alignment.topCenter,
+              )),
+              const SizedBox(height: 30),
+              ListTile(
+                  title: Text(
+                    trip.title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  subtitle: Text(
+                    '${trip.nights} night stay for only \$${trip.price}',
+                    style: const TextStyle(letterSpacing: 1),
+                  ),
+                  trailing: const Heart()),
+              Padding(
+                padding: const EdgeInsets.all(18),
+                child: Text(
+                  // lipsum.createText(numParagraphs: 1, numSentences: 3),
+                  lipsum.Ipsum().sentences(4),
+                  style: TextStyle(color: Colors.grey[600], height: 1.4),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
