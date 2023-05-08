9f837c6e7aa4dd6cfaaef75635fd4e44a1c0a021
Eklu Victor Dziedzorm <55409753+Victorspy-web@users.noreply.github.com>
2023-05-08 15:27:56 +0000

    tween sequence

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -9,7 +9,8 @@ class Heart extends StatefulWidget {
 
 class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-  late Animation _colorAnimation;
+  late Animation<Color?> _colorAnimation;
+  late Animation<double?> _sizeAnimation;
 
   // bool isFav = false;
 
@@ -19,12 +20,25 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
 
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 500),
+      duration: const Duration(milliseconds: 300),
     );
 
     _colorAnimation = ColorTween(
       begin: Colors.grey,
       end: Colors.red,
+    ).animate(_controller);
+
+    _sizeAnimation = TweenSequence(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 30, end: 50),
+          weight: 50,
+        ),
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 50, end: 30),
+          weight: 50,
+        ),
+      ],
     ).animate(_controller);
 
     // _controller.addListener(() {
@@ -58,7 +72,7 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
           icon: Icon(
             Icons.favorite,
             color: _colorAnimation.value,
-            size: 30,
+            size: _sizeAnimation.value,
           ),
           onPressed: () {
             _controller.isCompleted ? _controller.reverse() : _controller.forward();
